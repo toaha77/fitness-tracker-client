1dfa0e281f927e4847d2bbb6d519b59de07039e0
toaha77 <istiaktoaha07@gmail.com>
2024-03-16 14:38:27 +0600

    about section added

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,71 +1,26 @@
-import { AiOutlineArrowRight, AiOutlineHome } from "react-icons/ai";
-import { TbHomeDollar } from "react-icons/tb";
-import { FaHandshake } from "react-icons/fa";
-
 const About = () => {
   return (
     <div className="pt-8">
-      <div className="container mx-auto text-center ">
-        <h2 className="text-green-500 btn btn-sm  rounded-full text-xl">
-          About Us
-        </h2>
-        <h3 className="mt-2 text-4xl font-bold">Our Main Focus</h3>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <div className="mt-4 bg-gray-100 rounded-lg">
-        <div className="card  shadow-2xl">
-          <figure className="px-10 pt-10">
-            <AiOutlineHome className="text-6xl"></AiOutlineHome>
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="text-center text-2xl font-semibold">Buy a home</h2>
-            <p>
-              over 1 million+ homes for sale available on the website, we can
-              match you with a house you will want to call home.
-            </p>
-            <div className="flex items-center justify-center gap-1">
-              <h2 className="text-green-500">Find a Home </h2>
-              <AiOutlineArrowRight className="text-green-500"></AiOutlineArrowRight>
-            </div>
-          </div>
+      <div className="container mx-auto text-center "></div>
+      <div className="flex flex-col lg:flex-row gap-4">
+        <div className="lg:w-1/2 flex items-end justify-end">
+          <img
+            className="w-[400px] h-[400px]"
+            src="https://designingmedia.com/xtremefitness/wp-content/uploads/2022/03/about-us-image.jpg"
+            alt=""
+          />
         </div>
-      </div>
-      <div className="mt-4 bg-gray-100 rounded-lg">
-        <div className="card  shadow-2xl">
-          <figure className="px-10 pt-10">
-            <TbHomeDollar className="text-6xl"></TbHomeDollar>
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="text-center text-2xl font-semibold">Rent a home</h2>
-            <p>
-              over 1 million+ homes for sale available on the website, we can
-              match you with a house you will want to call home.
-            </p>
-            <div className="flex items-center justify-center gap-1">
-              <h2 className="text-green-500">Find a Home </h2>
-              <AiOutlineArrowRight className="text-green-500"></AiOutlineArrowRight>
-            </div>
-          </div>
+        <div className="lg:w-1/2">
+          <h2 className="text-orange-500 text-3xl">About Us</h2>
+
+          <p className="text-5xl">WELCOME TO THE REVITALIZE FITNESS</p>
+          <p>
+            At REVITALIZE, we go beyond traditional fitness. With
+            state-of-the-art facilities, experienced trainers, and a supportive
+            community, we are committed to helping you achieve holistic
+            well-being.
+          </p>
         </div>
-      </div>
-      <div className="mt-4 bg-gray-100 rounded-lg">
-        <div className="card  shadow-2xl">
-          <figure className="px-10 pt-10">
-            <FaHandshake className="text-6xl"></FaHandshake>
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="text-center text-2xl font-semibold">Sell a home</h2>
-            <p>
-              over 1 million+ homes for sale available on the website, we can
-              match you with a house you will want to call home.
-            </p>
-            <div className="flex items-center justify-center gap-1">
-              <h2 className="text-green-500">Find a Home </h2>
-              <AiOutlineArrowRight className="text-green-500"></AiOutlineArrowRight>
-            </div>
-          </div>
-        </div>
-      </div>
       </div>
     </div>
   );
