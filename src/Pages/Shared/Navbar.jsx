14ded08415b34b4d3dae55ea3758d76b43107abf
toaha77 <istiaktoaha07@gmail.com>
2024-03-16 11:16:11 +0600

    navbar added

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -1,205 +1,164 @@
-import { useContext, useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { HiOutlineBars3BottomRight } from "react-icons/hi2";
+import { LiaTimesSolid } from "react-icons/lia";
 import { Link, NavLink } from "react-router-dom";
-
-import { GiHomeGarage } from "react-icons/gi";
-import { AuthContext } from "../../Providers/AuthProvider";
-import { FaShoppingCart } from "react-icons/fa";
-import UseCart from "../../Hooks/UseCart";
-
-const Navbar = () => {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
-  );
-  const handleToggle = (e) => {
-    if (e.target.checked) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
+ import useAuth from "../../Hooks/UseAuth";
+const NavBar = () => {
+  const variants = {
+    open: { opacity: 1, x: 0 },
+    closed: { opacity: 0, x: "-100%" },
   };
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-    const localTheme = localStorage.getItem("theme");
-    document.querySelector("html").setAttribute("data-theme", localTheme);
-  }, [theme]);
-  const [fix, setFix] = useState(null);
-  const [cart] = UseCart()
-  const { user, logOut } = useContext(AuthContext);
 
-  useEffect(() => {
-    window.addEventListener("scroll", stickNavbar);
-    return () => {
-      window.removeEventListener("scroll", stickNavbar);
-    };
-  }, []);
-  const stickNavbar = () => {
-    if (window !== undefined) {
-      let windowHe = window.scrollY;
-      windowHe > 200 ? setFix("fixed top-0 left-0 z-50") : setFix("relative");
-    }
-  };
+  const {user, logOut} = useAuth()
+
   const navLinks = (
     <>
       <li>
         <NavLink
           to="/ "
           className={({ isActive, isPending }) =>
-            isPending
-              ? "pending"
-              : isActive
-              ? "text-green-500 rounded-md font-semibold border"
-              : ""
+            isPending ? "pending" : isActive ? " font-bold text-orange-400" : ""
           }
         >
           Home
         </NavLink>
       </li>
+     
+
       <li>
         <NavLink
-          to="/apartment "
+          to="/trainer"
           className={({ isActive, isPending }) =>
-            isPending
-              ? "pending"
-              : isActive
-              ? " text-green-500 rounded-md font-semibold   border"
-              : "  "
+            isPending ? "pending" : isActive ? "font-bold border-b-2" : ""
           }
         >
-          Apartment
+          Trainer
         </NavLink>
       </li>
       <li>
         <NavLink
-          to="/dashboard/cart"
+          to="/classes"
           className={({ isActive, isPending }) =>
-            isPending
-              ? "pending"
-              : isActive
-              ? " text-green-500 rounded-md font-semibold   border"
-              : "  "
+            isPending ? "pending" : isActive ? "font-bold border-b-2" : ""
           }
         >
-<button className="btn btn-sm">
-  <FaShoppingCart></FaShoppingCart>
-  <div className="badge badge-secondary">+{cart.length}</div>
-</button>
+          Classes
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/gallery"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "font-bold text-orange-400" : ""
+          }
+        >
+           Gallery
+        </NavLink>
+      </li>
+      
+      <li>
+        <NavLink
+          to="/contact"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "font-bold border-b-2" : ""
+          }
+        >
+          Contact Us
         </NavLink>
       </li>
     </>
   );
+  const [navbar, setNavbar] = useState(false);
   return (
-    <div className={`h-16 bg-white w-full ${fix}`}>
-      <div className=" navbar shadow-lg">
-        <div className=" navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu-sm dropdown-content mt-3 z-[5] p-2  shadow bg-base-100 rounded-box w-52"
-            >
-              {navLinks}
-            </ul>
-          </div>
-          <div className="flex items-center justify-center">
-            <Link to="/">
-              <div className="flex items-center justify-center gap-1">
-                <GiHomeGarage className="text-2xl text-green-500"></GiHomeGarage>
-                <h2 className="text-2xl uppercase">
-                  Rental<span className="text-green-500">place</span>
-                </h2>
-              </div>
-            </Link>
-          </div>
+    <div className=" border-b-2 text-white relative">
+      {/* For Mobile View  */}
+      <motion.div
+        animate={navbar ? "open" : "closed"}
+        variants={variants}
+        transition={{ type: "tween" }}
+        className={`fixed h-screen  bg-green-700 font-semibold top-0 px-8 py-8 md:hidden flex flex-col gap-4 z-50 
+        ${navbar ? "left0" : "-translate-x-full"}
+        `}
+      >
+        {navLinks}
+      </motion.div>
+      <nav className="w-[85%] md:w-[95%] lg:w-[100%] mx-auto flex items-center justify-between py-2 text-black font-semibold bg-gray-700">
+        <img
+          className="w-20 md:w-28 lg:w-24 px-4 py-1 rounded-full"
+          src='https://template-kit.evonicmedia.com/layout63/wp-content/uploads/2024/02/Group-1000002965-1.png'
+          alt=""
+        />
+        <div className="hidden md:flex items-center justify-center list-none gap-4">
+          {navLinks}
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className=" menu menu-horizontal  font-semibold px-1">
-            {navLinks}
-          </ul>
-        </div>
+        
 
-        <div className="navbar-end">
-          {user?.email ? (
+<div>
+          {user ? (
+            <div className="flex items-center gap-2">
             <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <img src={user?.photoURL} alt={user?.displayName} />
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img src={user.photoURL} alt={user.displayName} />
+                                </div>
+                            </label>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-52">
+                                <li>
+                                    <button className="btn btn-sm  btn-ghost">{user.displayName}</button>
+
+                                </li>
+                                <li>
+                                    <button className="btn btn-sm  btn-ghost"
+                                        onClick={logOut}
+                                    >Logout</button>
+
+                                </li>
+                            </ul>
+                        </div>
+                            
+              <div className="block md:hidden">
+                <div
+                  onClick={() => {
+                    setNavbar(!navbar);
+                  }}
+                  className=""
+                >
+                  {navbar ? (
+                    <LiaTimesSolid className="text-2xl duration-300 active:scale-75 cursor-pointer select-none"></LiaTimesSolid>
+                  ) : (
+                    <HiOutlineBars3BottomRight className="text-2xl duration-300 active:scale-75 cursor-pointer select-none"></HiOutlineBars3BottomRight>
+                  )}
                 </div>
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 z-[3]  md:z-[5] p-2 shadow bg-base-100 rounded-box w-52"
-              >
-                <li>
-                  <button className="btn btn-sm  btn-ghost">
-                    {user?.displayName}
-                  </button>
-                </li>
-                <li>
-                 <Link to='/dashboard/profile'>
-                 <button className="btn btn-sm  btn-ghost">
-                    Dashboard
-                  </button>
-                 </Link>
-                </li>
-                <li>
-                  <button className="btn btn-sm  btn-ghost" onClick={logOut}>
-                    Logout
-                  </button>
-                </li>
-              </ul>
+              </div>
             </div>
           ) : (
-            <Link to="/login">
-              <button className="btn btn-sm  btn-ghost">Login</button>
-            </Link>
+            <div className="flex items-center gap-2">
+              <Link to="login">
+                <p className="btn btn-sm px-3 py-1 rounded-full duration-300 select-none active:scale-90">
+                  Login
+                </p>
+              </Link>
+              <div className="block md:hidden">
+                <div
+                  onClick={() => {
+                    setNavbar(!navbar);
+                  }}
+                  className=""
+                >
+                  {navbar ? (
+                    <LiaTimesSolid className="text-2xl duration-300 active:scale-75 cursor-pointer select-none"></LiaTimesSolid>
+                  ) : (
+                    <HiOutlineBars3BottomRight className="text-2xl duration-300 active:scale-75 cursor-pointer select-none"></HiOutlineBars3BottomRight>
+                  )}
+                </div>
+              </div>
+            </div>
           )}
         </div>
-        <div className="flex justify-center items-center ml-2">
-          <label className="swap swap-rotate">
-            {/* this hidden checkbox controls the state */}
-            <input
-              type="checkbox"
-              onChange={handleToggle}
-              checked={theme === "light" ? false : true}
-            />
-
-            {/* sun icon */}
-            <svg
-              className="swap-on fill-current w-10 h-10"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-            </svg>
-
-            {/* moon icon */}
-            <svg
-              className="swap-off fill-current w-10 h-10"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-            </svg>
-          </label>
-        </div>
-      </div>
+      </nav>
     </div>
   );
 };
 
-export default Navbar;
+export default NavBar;
